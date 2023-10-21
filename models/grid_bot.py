fbfbaee1ae21c5d66e5e922891c6896f02a32706
Viroslav <ivanov.2017@stud.nstu.ru>
2023-10-21 22:05:44 +0300

    fixed returning self

--- models/grid_bot.py
+++ models/grid_bot.py
@@ -1,10 +1,5 @@
-from tqdm import tqdm
-import pandas as pd
-import matplotlib.pyplot as plt
-
-
 class GridBot:
-    def __init__(self, levels_num=10, balance=100000, levels_step=0.001, fee=0.2181 / 100):
+    def __init__(self, levels_num=10, balance=100000, levels_step=0.001, fee=0.2181 / 100, part=5, use_up=False):
         assert 0 < levels_step < 1, f'levels_step must be in (0, 1) range, {levels_step}'
         assert levels_num >= 1, f'levels_num must be greater than 0, {levels_num}'
 
@@ -12,13 +7,15 @@ class GridBot:
         self.levels_step = levels_step
         self.balance = balance
         self.history = []
-        self.assets = [0]
         self.ref_price = None
         self.buy_orders = []
         self.sell_orders = []
         self.fee = fee
         self.buy_points = []
         self.sell_points = []
+        self.use_up = use_up
+        self.assets_list = []
+        self.part = part
         self.bought = [False for _ in range(self.levels_num)]
 
     def trade(self, df_path):
@@ -27,7 +24,10 @@ class GridBot:
         ref_price = df.iloc[0]['Open']
         self.ref_price = ref_price
         buy_orders = [ref_price * (1 - k * self.levels_step) for k in range(1, self.levels_num + 1)]
-        sell_orders = [ref_price * (1 - k * self.levels_step) for k in range(self.levels_num)]
+        if self.use_up:
+            sell_orders = [ref_price * (1 + k * self.levels_step) for k in range(self.levels_num)]
+        else:
+            sell_orders = [ref_price * (1 - k * self.levels_step) for k in range(self.levels_num)]
         self.buy_orders = buy_orders
         self.sell_orders = sell_orders
         
@@ -36,18 +36,18 @@ class GridBot:
             for i in range(len(self.bought)):
                 if self.bought[i] and row.High >= sell_orders[i]:
                     self.bought[i] = False
-                    self.balance += (sell_orders[i] - self.fee*sell_orders[i])
-                    self.assets  += [ self.assets[-1] - sell_orders[i] / row.High]
+                    self.balance += (self.assets_list[-1] * sell_orders[i] -self.assets_list[-1] * sell_orders[i] * self.fee)
+                    del self.assets_list[-1]
                     self.history += [self.balance]
-                    self.sell_points += [(index, row.High)]
+                    self.sell_points += [(index, sell_orders[i])]
                     
-                elif not self.bought[i] and row.Low <= buy_orders[i] and self.balance > row.Low:
+                elif not self.bought[i] and row.Low <= buy_orders[i] and self.balance >= self.balance / self.part:
                     self.bought[i] = True
-                    self.balance -= (buy_orders[i] + self.fee * buy_orders[i])
-                    self.assets  += [(buy_orders[i] - self.fee * buy_orders[i]) / row.Low]
+                    self.balance -= (self.balance / self.part + self.fee * self.balance / self.part)
                     self.history += [self.balance]
-                    self.buy_points += [(index, row.Low)]
-                    
+                    self.buy_points += [(index, buy_orders[i])]
+                    self.assets_list += [(self.balance/ self.part- self.fee * self.balance / self.part) / buy_orders[i]]
+
         return self
                     
                     
