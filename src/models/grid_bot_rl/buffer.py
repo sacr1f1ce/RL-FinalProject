9d81b2e7b624d308f56797768749cc22f62bc0a5
MarkN <nuzhnov.ms@phystech.edu>
2023-10-23 00:03:40 +0300

    features, remove trash + rl begining

---
+++ src/models/grid_bot_rl/buffer.py
@@ -0,0 +1,139 @@
+class IterationBuffer(Dataset):
+    """Buffer for experience replay"""
+
+    def __init__(self) -> None:
+        """Initialize `IterationBuffer`"""
+
+        super().__init__()
+        self.next_baselines = None
+        self.nullify_buffer()
+
+    def nullify_buffer(self) -> None:
+        """Clear all buffer data"""
+
+        self.episode_ids = []
+        self.observations = []
+        self.actions = []
+        self.running_objectives = []
+        self.step_ids = []
+        self.total_objectives = None
+        self.baselines = None
+
+    def add_step_data(
+        self,
+        observation: np.array,
+        action: np.array,
+        running_objective: float,
+        step_id: int,
+        episode_id: int,
+    ):
+        """Add step data to experience replay
+
+        Args:
+            observation (np.array): current observation
+            action (np.array): current action
+            running_objective (float): current running objective
+            step_id (int): current step
+            episode_id (int): current episode
+        """
+        self.observations.append(observation)
+        self.actions.append(action)
+        self.running_objectives.append(running_objective)
+        self.episode_ids.append(int(episode_id))
+        self.step_ids.append(step_id)
+
+    def get_N_episodes(self) -> int:
+        """Get number of episodes
+
+        Returns:
+            int: number of episodes
+        """
+        return len(np.unique(self.episode_ids))
+
+    def calculate_tail_total_objectives_and_next_baselines(
+        self,
+    ) -> Tuple[np.array, float, float]:
+        """Calculate tail total costs and baseline
+
+        Returns:
+            Tuple[np.array, float, float]: tuple of 3 elements tail_total_objectives, baseline, gradent_normalization_constant
+        """
+
+        unique_episode_ids = np.unique(self.episode_ids)
+        running_objectives_series = pd.Series(
+            index=self.episode_ids, data=self.running_objectives
+        )
+
+        tail_total_objectives = pd.concat(
+            [
+                running_objectives_series.loc[i][::-1].cumsum()[::-1]
+                for i in unique_episode_ids
+            ]
+        ).values.reshape(-1)
+
+        next_baselines = (
+            pd.Series(index=self.step_ids, data=tail_total_objectives)
+            .groupby(level=0)
+            .mean()
+            .loc[self.step_ids]
+            .values.reshape(-1)
+        )
+
+        return tail_total_objectives, next_baselines
+
+    def __len__(self) -> int:
+        """Get length of buffer. The method should be overrided due to inheritance from `torch.utils.data.Dataset`
+
+        Returns:
+            int: length of buffer
+        """
+        return len(self.observations)
+
+    def __getitem__(self, idx: int) -> Dict[str, torch.tensor]:
+        """Get item with id `idx`. The method should be overrided due to inheritance from `torch.utils.data.Dataset`
+
+        Args:
+            idx (int): id of dataset item to return
+
+        Returns:
+            Dict[str, torch.tensor]: dataset item, containing catted observation-action, tail total objective and baselines
+        """
+
+        if self.total_objectives is None:
+            self.baselines = (
+                self.next_baselines
+                if self.next_baselines is not None
+                else np.zeros(shape=len(self.observations))
+            )
+
+            (
+                self.total_objectives,
+                self.next_baselines,
+            ) = self.calculate_tail_total_objectives_and_next_baselines()
+
+        observation = torch.tensor(self.observations[idx])
+        action = torch.tensor(self.actions[idx])
+
+        return {
+            "observations_actions": torch.cat([observation, action]),
+            "tail_total_objectives": torch.tensor(self.total_objectives[idx]),
+            "baselines": torch.tensor(self.baselines[idx]),
+        }
+
+    @property
+    def data(self) -> pd.DataFrame:
+        """Return current buffer content in pandas.DataFrame
+
+        Returns:
+            pd.DataFrame: current buffer content
+        """
+
+        return pd.DataFrame(
+            {
+                "episode_id": self.episode_ids,
+                "step_id": self.step_ids,
+                "observation": self.observations,
+                "action": self.actions,
+                "running_objective": self.running_objectives,
+            }
+        )
